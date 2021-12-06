e0d07bfc6798d69a72a8dafc528e6f1dd75a7a57
YAO Cyrille <cyaoyapi@gmail.com>
2021-12-06 03:36:39 +0000

    Variables declaration and assignment

---
+++ script.js
@@ -0,0 +1,5 @@
+let numberOfSeasons = 6;
+let numberOfEpisodes = 12;
+
+let paragraph = document.querySelector("#info");
+paragraph.innerHTML = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per seasons.`;
