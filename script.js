6ab4f04a6cf275559bfb1f5e81dbfb1cc29a6f82
YAO Cyrille <cyaoyapi@gmail.com>
2021-12-06 04:25:41 +0000

    Coveying a number of days in hours, minutes and seconds

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let numberOfSeasons = 6;
-let numberOfEpisodes = 12;
-let episodeTime = 45;
-let commercialTime = 5;
-let totalShowTime = (episodeTime + commercialTime) * numberOfEpisodes * numberOfSeasons
+const hoursPerDay = 24;
+const minutesPerHour = 60;
+const secondsPerMinute = 60;
 
-let paragraph = document.querySelector("#info");
-paragraph.innerHTML = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per seasons.
-Total viewing time : ${totalShowTime}.`;
+const daysInput = document.querySelector("#days-input")
+const calculateButton = document.querySelector("#calculate-button")
+const hours = document.querySelector("#hours")
+const minutes = document.querySelector("#minutes")
+const seconds = document.querySelector("#seconds")
+
+calculateButton.addEventListener('click', () => {
+    let days = daysInput.value;
+    let calculateHours = days * hoursPerDay;
+    let calculateMinutes = calculateHours * minutesPerHour;
+    let calculateSeconds = calculateMinutes * secondsPerMinute;
+    hours.innerHTML = `${calculateHours} hours`;
+    minutes.innerHTML = `${calculateMinutes} minutes`;
+    seconds.innerHTML = `${calculateSeconds} seconds`;
+}
+)
