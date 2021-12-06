005231eb5247286b5ca16e9200bbe2568a2267e6
YAO Cyrille <cyaoyapi@gmail.com>
2021-12-06 03:53:05 +0000

    Basic mathematic operations

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let numberOfSeasons = 6;
 let numberOfEpisodes = 12;
+let episodeTime = 45;
+let commercialTime = 5;
+let totalShowTime = (episodeTime + commercialTime) * numberOfEpisodes * numberOfSeasons
 
 let paragraph = document.querySelector("#info");
-paragraph.innerHTML = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per seasons.`;
+paragraph.innerHTML = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per seasons.
+Total viewing time : ${totalShowTime}.`;
