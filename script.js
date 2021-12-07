7b8a68fb1175275e44ddad4a9dc07310eafeec53
YAO Cyrille <cyaoyapi@gmail.com>
2021-12-07 16:13:13 +0000

    Arrays

--- script.js
+++ script.js
@@ -9,15 +9,20 @@ class Episode{
   let firstEpisode = new Episode("My name is", 10, true);
   let secondEpisode = new Episode("How are you ?", 30, false);
   let thirdEpisode = new Episode("Where are come from ?", 20, true);
+  let episodes = [firstEpisode, secondEpisode, thirdEpisode]
+
+  const body = document.querySelector("body");
   
-  document.querySelector('#first-episode-info').innerText = `Episode: ${firstEpisode.title}
-  Duration: ${firstEpisode.duration} min
-  ${firstEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
-  
-  document.querySelector('#second-episode-info').innerText = `Episode: ${secondEpisode.title}
-  Duration: ${secondEpisode.duration} min
-  ${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
-  
-  document.querySelector('#third-episode-info').innerText = `Episode: ${thirdEpisode.title}
-  Duration: ${thirdEpisode.duration} min
-  ${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+  for(let i = 0; i < episodes.length; i++){
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("series-frame");
+        let newTitle = document.createElement("h2");
+        newTitle.innerHTML = "The Story of Tau";
+        let newParagraph = document.createElement("p");
+        newParagraph.innerHTML = `${episodes[i].title}. 
+        ${episodes[i].duration} minutes.
+        ${episodes[i].hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+        newDiv.append(newTitle);
+        newDiv.append(newParagraph);
+        body.append(newDiv);
+    }
