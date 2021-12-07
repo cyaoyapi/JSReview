44b9fdb4fd72d6caf21d08ec12f7a06514e1d8d1
YAO Cyrille <cyaoyapi@gmail.com>
2021-12-07 19:20:25 +0000

    comparaison operations

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-const episode = {
-    title: "Modern events made clear by prophecy",
-    duration: 30,
-    hasBeenWatched: true
-};
-let episodeTitle = episode.title;
-let episodeDuration = episode.duration;
-let hasBeenWatched = episode.hasBeenWatched;
-const episodeInfo = document.querySelector("#episode-info");
-episodeInfo.innerHTML = `Episode : ${episodeTitle}. 
-Duratiion : ${episodeDuration}. 
-${hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+let age;
+const ageOfMajority = 18;
+const submitButton = document.getElementById("submitButton");
+const message = document.getElementById("message");
+function forbid(){
+    alert("This place is forbidden to minors");
+}
+
+function accept(){
+    message.innerHTML = "Get in. You are major.";
+}
+let parentalControl = document.querySelector("#parental-control");
+let inputAge = document.getElementById("input-age");
+
+function onSubmit(){
+    if(isNaN(inputAge.value)){
+        alert("The age must be a number!");
+        return;
+    }
+    age = ParseInt(inputAge.value);
+    if((parentalControl.checked && (age >= ageOfMajority)) || !parentalControl.checked){
+        accept();
+    } else{
+        forbid();
+    }
+    inputAge.value = "";
+}
+
+submitButton.addEventListener('click', onSubmit);
