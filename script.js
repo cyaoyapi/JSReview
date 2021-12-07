b475b09541fd4475af88dadce4d7f0dc35d7a266
YAO Cyrille <cyaoyapi@gmail.com>
2021-12-07 10:43:27 +0000

    Classes

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const episode = {
-    title: "Modern events made clear by prophecy",
-    duration: 30,
-    hasBeenWatched: true
-};
-let episodeTitle = episode.title;
-let episodeDuration = episode.duration;
-let hasBeenWatched = episode.hasBeenWatched;
-const episodeInfo = document.querySelector("#episode-info");
-episodeInfo.innerHTML = `Episode : ${episodeTitle}. 
-Duratiion : ${episodeDuration}. 
-${hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+class Episode{
+    constructor(title, duration, hasBeenWatched){
+      this.title = title;
+      this.duration = duration;
+      this.hasBeenWatched = hasBeenWatched;
+    }
+  }
+  
+  let firstEpisode = new Episode("My name is", 10, true);
+  let secondEpisode = new Episode("How are you ?", 30, false);
+  let thirdEpisode = new Episode("Where are come from ?", 20, true);
+  
+  document.querySelector('#first-episode-info').innerText = `Episode: ${firstEpisode.title}
+  Duration: ${firstEpisode.duration} min
+  ${firstEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+  
+  document.querySelector('#second-episode-info').innerText = `Episode: ${secondEpisode.title}
+  Duration: ${secondEpisode.duration} min
+  ${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+  
+  document.querySelector('#third-episode-info').innerText = `Episode: ${thirdEpisode.title}
+  Duration: ${thirdEpisode.duration} min
+  ${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
