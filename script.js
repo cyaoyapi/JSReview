6437f46f5d91d42acda01d60a3b400f67d71cd44
YAO Cyrille <cyaoyapi@gmail.com>
2021-12-07 10:33:15 +0000

    Dotnet notation

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let episode = {
+const episode = {
     title: "Modern events made clear by prophecy",
     duration: 30,
     hasBeenWatched: true
 };
-
+let episodeTitle = episode.title;
+let episodeDuration = episode.duration;
+let hasBeenWatched = episode.hasBeenWatched;
 const episodeInfo = document.querySelector("#episode-info");
-episodeInfo.innerHTML = `Episode : ${episode.title}. 
-Duratiion : ${episode.duration}. 
-${episode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+episodeInfo.innerHTML = `Episode : ${episodeTitle}. 
+Duratiion : ${episodeDuration}. 
+${hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
