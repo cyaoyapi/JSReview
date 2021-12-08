6091b0a54ab342d2b92c390d362006e5b39b7d11
YAO Cyrille <cyaoyapi@gmail.com>
2021-12-08 17:32:58 +0000

    functions

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 class Episode{
-    constructor(title, duration, hasBeenWatched){
+    constructor(title, duration, hasBeenWatched, ratings){
       this.title = title;
       this.duration = duration;
       this.hasBeenWatched = hasBeenWatched;
+      this.ratings = ratings;
     }
   }
+
+  const calculateRatingsAverage = (ratings) => {
+      if(ratings.length === 0){
+        return 0;
+      } else{
+        let sumOfRatinngs = 0;
+        for(let rating of ratings){
+          sumOfRatinngs += rating;
+        }
+        const averageOfratings = sumOfRatinngs / ratings.length;
+        return averageOfratings;
+      }
+  };
   
-  let firstEpisode = new Episode("My name is", 10, true);
-  let secondEpisode = new Episode("How are you ?", 30, false);
-  let thirdEpisode = new Episode("Where are come from ?", 20, true);
+  let firstEpisode = new Episode("My name is", 10, true, [2, 4, 1, 5]);
+  let secondEpisode = new Episode("How are you ?", 30, false, [5, 5, 3]);
+  let thirdEpisode = new Episode("Where are come from ?", 20, true, []);
   let episodes = new Array();
   episodes.push(firstEpisode);
   episodes.push(secondEpisode);
@@ -24,6 +38,8 @@ class Episode{
         let newParagraph = document.createElement("p");
         newParagraph.innerHTML = `${episode.title}. 
         ${episode.duration} minutes.
+        Average of ratings : ${calculateRatingsAverage(episode.ratings) !== 0 ? 
+          calculateRatingsAverage(episode.ratings).toFixed(1) : "Not yet ratings."}
         ${episode.hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
         newDiv.append(newTitle);
         newDiv.append(newParagraph);
